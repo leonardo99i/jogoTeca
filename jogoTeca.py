67672003504ae2e9e025ff3d298bafbd7a9e4cab
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-05-15 14:22:45 -0300

    Update jogoTeca.py

--- jogoTeca.py
+++ jogoTeca.py
@@ -1,10 +1,9 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return render_template('/lista.html')
+    return render_template('lista.html');
 
 app.run()
